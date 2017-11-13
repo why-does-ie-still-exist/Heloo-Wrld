9437eef3d2d047a258bd9e9661e58e194d70d797
King Kale <33632547+why-does-ie-still-exist@users.noreply.github.com>
2017-11-13 14:22:14 -0800

    Update Hi.java

--- Hi.java
+++ Hi.java
@@ -2,7 +2,8 @@ package hi;
 public class Hi {
 //sudo apt-get wtf
     public static void main(String[] args) {
-        System.out.println("Heloo Wrld");
-        System.exit(0)
+        System.out.println("We be out here");
+        System.out.println("Respecting women");
+        System.exit(0);
     }
 }
